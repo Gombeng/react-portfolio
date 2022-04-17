d5b4e255a8a6b51b9747512fe2156f4406e65826
Gombeng <msrgombeng@gmail.com>
2022-04-17 17:10:59 +0700

    setup folder and basic styling

---
+++ src/pages/about/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<div className="container">
+			<h1>This is About page</h1>
+		</div>
+	);
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+	return (
+		<div className="container">
+			<h1>I'm Syahrizal Ardana</h1>
+			<p>Front-end Developer</p>
+
+			<button>Download CV</button>
+		</div>
+	);
+}
