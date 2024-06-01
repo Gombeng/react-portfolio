047ebddcd413ba5d925c3dacd84fc4127635a24d
ardana <msrgombeng@gmail.com>
2024-06-01 13:48:11 +0700

    Add project

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,3 +1,6 @@
+const baseUrlGithub = "https://github.com/Gombeng";
+const basePathImage = "./images/project-";
+
 export const data = {
   home: {
     name: {
@@ -11,52 +14,59 @@ export const data = {
   },
   works: [
     {
-      img: require("./images/project-moviedb.png"),
+      img: require(`${basePathImage}web-gis.png`),
+      title: "Geographic Information System Website",
+      desc: "Tour & Drug Spread at Tanjungpinang (dummy shp data)",
+      demoUrl: "https://web-gis-react.vercel.app/",
+      repoUrl: `${baseUrlGithub}web-gis-react`,
+    },
+    {
+      img: require(`${basePathImage}moviedb.png`),
       title: "Movie DB",
       desc: "Movie DB website create using ViteJS",
       demoUrl: "https://movie-db-lake.vercel.app/",
-      repoUrl: "https://github.com/Gombeng/movie-db",
+      repoUrl: `${baseUrlGithub}movie-db`,
     },
     {
-      img: require("./images/project-inforsys-indonesia.png"),
+      img: require(`${basePathImage}inforsys-indonesia.png`),
       title: "Inforsys Indonesia",
       desc: "Landing page website create using AngularJS",
       demoUrl: "https://gombeng-lp-inforsys.netlify.app/",
-      repoUrl: "https://github.com/Gombeng/inforsys",
+      repoUrl: `${baseUrlGithub}inforsys`,
     },
     {
-      img: require("./images/project-porto.png"),
+      img: require(`${basePathImage}porto.png`),
       title: "React Portfolio",
       desc: "Portfolio website created using React JS",
       demoUrl: "https://syahrizal-portfolio.vercel.app/",
-      repoUrl: "https://github.com/Gombeng/react-portfolio",
+      repoUrl: `${baseUrlGithub}react-portfolio`,
     },
     {
-      img: require("./images/project-restapi.png"),
+      img: require(`${basePathImage}restapi.png`),
       title: "Rest Country API",
       desc: "Website to search all the information about country all over the world!",
-      repoUrl: "https://github.com/Gombeng/Rest-Country-API",
+      repoUrl: `${baseUrlGithub}Rest-Country-API`,
       demoUrl: "https://rest-country-api-six.vercel.app/",
     },
     {
-      img: require("./images/project-denote.png"),
+      img: require(`${basePathImage}denote.png`),
       title: "Denote",
       desc: "Application to save your brilian idea, don't forget to signup first",
-      repoUrl: "https://github.com/Gombeng/denote-mern",
+      repoUrl: `${baseUrlGithub}denote-mern`,
       demoUrl: "https://denote-mern.vercel.app/signup",
     },
     {
-      img: require("./images/project-kitteni.png"),
+      img: require(`${basePathImage}kitteni.png`),
       title: "Kitteni",
       desc: "Website contain all the cute cat around the world, you can see the detail too",
-      repoUrl: "https://github.com/Gombeng/kitteni",
+      repoUrl: `${baseUrlGithub}kitteni`,
       demoUrl: "https://kitteni.vercel.app/",
     },
     {
-      img: require("./images/project-quiz.png"),
+      img: require(`${basePathImage}quiz.png`),
       title: "Easely Quiz",
       desc: "Play some quick quiz game to test your inteligence",
-      repoUrl: "https://github.com/Gombeng/easely-quiz",
+      repoUrl: `${baseUrlGithub}easely-quiz`,
       demoUrl: "https://easely-quiz.vercel.app/",
     },
   ],
